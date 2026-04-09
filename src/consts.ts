fd965634c7bec2d9c9573fc4e154497715149cd1
Cristóbal Ramos Merino <cristobalramosmerino@gmail.com>
2026-04-09 14:12:23 -0400

    feat: basic sections and under construction

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,3 @@
 export const SITE_TITLE = 'Cosmopolitican';
 export const SITE_DESCRIPTION = 'Tu revista con clase';
+export const SHOW_UNDER_CONSTRUCTION = true; // Cambia a false para mostrar la página normalmente
